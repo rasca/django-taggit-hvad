6351113a6efceea189421fe8175620339ba72377
Iván Raskovsky <raskovsky+git@gmail.com>
2012-05-20 17:57:13 -0300

    added hvad in admin

--- taggit/admin.py
+++ taggit/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 
+from hvad.admin import TranslatableAdmin
 from taggit.models import Tag, TaggedItem
 
 
 class TaggedItemInline(admin.StackedInline):
     model = TaggedItem
 
-class TagAdmin(admin.ModelAdmin):
-    list_display = ["name"]
+class TagAdmin(TranslatableAdmin):
+    list_display = ('__unicode__', 'all_translations', )
     inlines = [
         TaggedItemInline
     ]
